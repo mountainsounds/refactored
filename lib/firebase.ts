3a6ea48b6e639c7044b88fd293d9a54bf6e08835
Zack Sawyer <mountainsounds15@gmail.com>
2022-12-31 17:56:52 -0500

    setup firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnDphKMnkah69HxE5YP615-yTY0QBSBKU",
+    authDomain: "refactored-32d58.firebaseapp.com",
+    projectId: "refactored-32d58",
+    storageBucket: "refactored-32d58.appspot.com",
+    messagingSenderId: "784558460067",
+    appId: "1:784558460067:web:9e92c396cc37d252530316",
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
