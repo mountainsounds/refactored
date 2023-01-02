9dbc8d7aa33d67056c414697e6799fc68e4d6060
Zack Sawyer <mountainsounds15@gmail.com>
2023-01-02 16:58:24 -0500

    create navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,43 @@
+import Link from "next/link";
+
 const Navbar = props => {
+    const user = null;
+    const username = null;
+
+    const renderNotValidated = () => (
+        <li>
+            <Link href='/enter'>
+                <button className="btn-blue">Log in</button>
+            </Link>
+        </li>
+    )
+
+    const renderValidated = () => (
+        <>
+            <li className="push-left">
+                <Link href='/admin'>
+                    <button className="btn-blue">Write Posts</button>
+                </Link>
+            </li>
+            <li>
+                <Link href={`/${username}`}>
+                    <img src={user?.photoURL} />
+                </Link>
+            </li>
+        </>
+    )
+
     return (
-        <main>
-        </main>
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <Link href="/">
+                        <button className="btn-logo">FEED</button>
+                    </Link>
+                </li>
+                {username ? renderValidated() : renderNotValidated()}
+            </ul>
+        </nav>
     )
 }
 
