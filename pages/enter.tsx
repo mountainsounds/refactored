b271bf3ef951414270ea13ef21ce5f4a0e2526de
Zack Sawyer <mountainsounds15@gmail.com>
2023-01-03 01:36:19 -0500

    enable google auth

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,8 +1,37 @@
-const EnterPage = props => {
+import { auth, googleAuthProvider } from '../lib/firebase';
+
+const Enter = props => {
+  const user = null;
+  const username = null;
+
+  if (!user) return <main><SignInButton /></main>
   return (
     <main>
+      {username ? <SignOutButton /> : <UsernameForm />}
     </main>
-  )
+  );
 }
 
-export default EnterPage;
+export default Enter;
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={'/google.png'} /> Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
+}
